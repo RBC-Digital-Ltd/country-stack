54504f436a5bbb140b6c1463c27c701d994c7cd7
Rahul Parkar <rahul.a.parkar@gmail.com>
2022-12-19 23:26:05 +0000

    Map Remix Run repos due to Jest sillyness

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,15 @@ module.exports = {
   testEnvironment: "jsdom",
   testPathIgnorePatterns: ["/playwright/", "/node_modules/"],
   setupFilesAfterEnv: ["<rootDir>/test/setup-after-env.ts"],
+  moduleNameMapper: {
+    // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
+    "@remix-run/web-fetch": require.resolve("@remix-run/web-fetch"),
+    "@remix-run/web-blob": require.resolve("@remix-run/web-blob"),
+    "@remix-run/web-stream": require.resolve("@remix-run/web-stream"),
+    "@remix-run/web-form-data": require.resolve("@remix-run/web-form-data"),
+    "@remix-run/web-file": require.resolve("@remix-run/web-file"),
+    "@web3-storage/multipart-parser": require.resolve(
+      "@web3-storage/multipart-parser"
+    ),
+  },
 };
